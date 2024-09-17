862b605062dbda8ea83681b7493ab0da0f973635
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-17 15:03:58 -0400

    fix error - Invalid reference

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -1,4 +1,3 @@
-
 locals {
   external_ids = {
     "646602658615" = "c161b4cc-b028-5e18-b2cb-7a81e37db881",
@@ -6,12 +5,10 @@ locals {
     "496028668001" = "68eb48ff-46be-5405-b824-3e2f40a70d04",
     "912326510691" = "af776340-5d83-5f05-99b1-4fd60691e641",
     "051315720372" = "22b7a8fb-f07d-5cd3-91b6-8efecdcdd25e"
-    }
-    default_external_id = "68eb48ff-46be-5405-b824-3e2f40a70d04"
-    external_id = external_ids[data.aws_caller_identity.current.account_id]==null?local.default_external_id:external_ids[data.aws_caller_identity.current.account_id]
+  }
+  default_external_id = "1cfe95fd-6b84-441d-a691-0a163797742a"
+  external_id = lookup(local.external_ids, data.aws_caller_identity.current.account_id, local.default_external_id)
 }
-
-
 
 module "uptycs_aws_cspm" {
   source             = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
