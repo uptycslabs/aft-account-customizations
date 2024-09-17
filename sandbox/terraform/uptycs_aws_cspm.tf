9d728a1966ee492776b1050109588c4039d8f176
Umesh <usirsiwal@uptycs.com>
2024-09-17 14:28:02 -0400

    Use right external ids

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -7,6 +7,8 @@ locals {
     "912326510691" = "af776340-5d83-5f05-99b1-4fd60691e641",
     "051315720372" = "22b7a8fb-f07d-5cd3-91b6-8efecdcdd25e"
     }
+    default_external_id = "68eb48ff-46be-5405-b824-3e2f40a70d04"
+    external_id = external_ids[data.aws_caller_identity.current.account_id]==null?local.default_external_id:external_ids[data.aws_caller_identity.current.account_id]
 }
 
 
@@ -16,5 +18,5 @@ module "uptycs_aws_cspm" {
 
   upt_account_id     = "685272795239"
   integration_prefix = "debug-aft-uptycs-cspm"
-  external_id        = local.external_ids[data.aws_caller_identity.current.account_id]
+  external_id        = local.external_id
 }
