9a05202b4a27a7cd193001ddffeef0a29fca664f
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-17 18:52:31 -0400

    added support for cloudtrail and agentless target

--- sandbox/terraform/uptycs_aws_cspm_aft.tf
+++ sandbox/terraform/uptycs_aws_cspm_aft.tf
@@ -1,20 +1,31 @@
 locals {
-  uptycs_integration = {
+  cspm_integration = {
     scannerAccountId   = "794888992839"
     external_id        = "1cfe95fd-6b84-441d-a691-0a163797742a"
     integrationPrefix  = "GiriAFTIntegrationRole"
-  }  // Update above values as required
-}
-
-module "uptycs_aws_agentless_target_aft" {
-  source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
-  scanner_account_id = local.uptycs_integration.scannerAccountId
-  integration_prefix = local.uptycs_integration.integrationPrefix
+  }
+  cloudtrail_integration = {
+    "912326510691" = ["aws-cloudtrail-logs-794888992839-e26dd11a"]
+  }
 }
 
 module "uptycs_aws_cspm_aft" {
   source             = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
   upt_account_id     = "685272795239"
-  external_id        = local.uptycs_integration.external_id
-  integration_prefix = local.uptycs_integration.integrationPrefix
+  external_id        = local.cspm_integration.external_id
+  integration_prefix = local.cspm_integration.integrationPrefix
+}
+
+module "uptycs_aws_audit_logs_aft" {
+  source                     = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/auditlog_integration/accounts"
+  upt_account_id             = "685272795239"
+  external_id                = local.cspm_integration.external_id
+  integration_prefix         = local.cspm_integration.integrationPrefix
+  cloudtrail_s3_bucket_names = lookup(local.cloudtrail_integration, data.aws_caller_identity.current.account_id, [])
+}
+
+module "uptycs_aws_agentless_target_aft" {
+  source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
+  scanner_account_id = local.cspm_integration.scannerAccountId
+  integration_prefix = local.cspm_integration.integrationPrefix
 }
