3e8e03f36807f070a38a6d38d9d91ce23973c11d
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-17 18:27:11 -0400

    Update uptycs_aws_cspm_aft.tf

--- sandbox/terraform/uptycs_aws_cspm_aft.tf
+++ sandbox/terraform/uptycs_aws_cspm_aft.tf
@@ -6,13 +6,13 @@ locals {
   }  // Update above values as required
 }
 
-module "uptycs_aws_agentless_target" {
+module "uptycs_aws_agentless_target_aft" {
   source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
   scanner_account_id = local.uptycs_integration.scannerAccountId
   integration_prefix = local.uptycs_integration.integrationPrefix
 }
 
-module "uptycs_aws_cspm" {
+module "uptycs_aws_cspm_aft" {
   source             = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
   upt_account_id     = "685272795239"
   external_id        = local.uptycs_integration.external_id
