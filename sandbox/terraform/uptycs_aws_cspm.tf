dc73ecfa8ddafca4f42d86f230ff2a99c2b6a2d7
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-17 07:47:14 -0400

    Updated uptycs_aws_cspm.tf

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -1,27 +1,22 @@
 locals {
   account_customizations = {
     "cloudquery-root-management" = {
-      account_id        = "646602658615"
       integrationPrefix = "debug-11"
       externalId        = "012e935c-45a7-5c63-936d-924967513211"
     },
     "cqmanagement-account-1" = {
-      account_id        = "924967513276"
       integrationPrefix = "debug-22"
       externalId        = "012e935c-45a7-5c63-936d-924967513222"
     },
     "cqmanagement-account-2" = {
-      account_id        = "496028668001"
       integrationPrefix = "debug-33"
       externalId        = "012e935c-45a7-5c63-936d-924967513233"
     },
     "childaccount2" = {
-      account_id        = "912326510691"
       integrationPrefix = "debug-44"
       externalId        = "012e935c-45a7-5c63-936d-924967513244"
     },
     "childaccount3" = {
-      account_id        = "051315720372"
       integrationPrefix = "debug-55"
       externalId        = "012e935c-45a7-5c63-936d-924967513255"
     }
@@ -35,8 +30,4 @@ module "uptycs_aws_cspm" {
   upt_account_id     = "685272795239"
   integration_prefix = each.value.integrationPrefix
   external_id        = each.value.externalId
-
-  providers = {
-    aws = aws."${each.value.account_id}"
-  }
 }
