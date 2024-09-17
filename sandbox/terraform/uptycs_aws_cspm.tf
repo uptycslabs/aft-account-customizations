b428a772391657e7e70517839846023757d59605
Umesh <usirsiwal@uptycs.com>
2024-09-17 11:16:00 -0400

    Changes to support per account external id.

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -1,7 +1,20 @@
+
+locals {
+  external_ids = {
+    "646602658615" = "012e935c-45a7-5c63-936d-924967513211",
+    "924967513276" = "012e935c-45a7-5c63-936d-924967513222",
+    "496028668001" = "012e935c-45a7-5c63-936d-924967513233",
+    "912326510691" = "012e935c-45a7-5c63-936d-924967513244",
+    "051315720372" = "012e935c-45a7-5c63-936d-924967513255"
+    }
+}
+
+
+
 module "uptycs_aws_cspm" {
   source             = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
 
   upt_account_id     = "685272795239"
   integration_prefix = "debug-aft-uptycs-cspm"
-  external_id        = "ed93345d-4f92-488a-89a1-42ae16d3b8a5"
+  external_id        = local.external_ids[data.aws_caller_identity.current.account_id]
 }
