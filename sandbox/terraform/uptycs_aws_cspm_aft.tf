72c4fb45af8d917d819527a2cb2b9346628b43da
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-18 13:34:15 -0400

    create agentless target iam roles through AFT

--- sandbox/terraform/uptycs_aws_cspm_aft.tf
+++ sandbox/terraform/uptycs_aws_cspm_aft.tf
@@ -4,3 +4,9 @@ module "uptycs_aws_cspm_aft" {
   integration_prefix = "GiriAFTIntegrationRole"  
   external_id        = "557a929b-39a5-4335-9fb0-c005f9cd44c3"
 }
+
+module "uptycs_aws_agentless_target_aft" {
+  source = "https://uptycs-terraform.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
+  integration_prefix = "GiriScannerIntegration"
+  scanner_account_id = "646602658615"
+}
