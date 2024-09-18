bd8d47610bf32ab82ea821770a009ab75a72c6d6
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-17 21:28:04 -0400

    Update uptycs_aws_cspm_aft.tf

--- sandbox/terraform/uptycs_aws_cspm_aft.tf
+++ sandbox/terraform/uptycs_aws_cspm_aft.tf
@@ -1,6 +1,5 @@
 locals {
   cspm_integration = {
-    scannerAccountId   = "794888992839"
     external_id        = "1cfe95fd-6b84-441d-a691-0a163797742a"
     integrationPrefix  = "GiriAFTIntegrationRole"
   }
@@ -22,10 +21,4 @@ module "uptycs_aws_audit_logs_aft" {
   external_id                = local.cspm_integration.external_id
   integration_prefix         = local.cspm_integration.integrationPrefix
   cloudtrail_s3_bucket_names = lookup(local.cloudtrail_integration, data.aws_caller_identity.current.account_id, [])
-}
-
-module "uptycs_aws_agentless_target_aft" {
-  source = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/agentless_integration/target"
-  scanner_account_id = local.cspm_integration.scannerAccountId
-  integration_prefix = local.cspm_integration.integrationPrefix
 }
