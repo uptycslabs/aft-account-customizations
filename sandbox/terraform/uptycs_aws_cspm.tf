3bef3b5738de2ceacbff7dc49efc236d7ebe1d01
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-26 14:18:18 -0400

    Update uptycs_aws_cspm.tf

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -7,9 +7,10 @@ locals {
   default_values = {
     external_id        = "557a929b-39a5-4335-9fb0-c005f9cd44c3"
     integration_prefix = "UptycsIntegrationRole"
-  } 
+  }
   account_info = lookup(local.cspm_integration, data.aws_caller_identity.current.account_id, local.default_values)
 }
+
 module "uptycs_aws_cspm" {
   source             = "https://uptycs-terraform.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration/accounts"
   upt_account_id     = "685272795239"
