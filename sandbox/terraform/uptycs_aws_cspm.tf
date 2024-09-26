1fa65367f82e4b8f858a9d40706da6f39ec5f859
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-09-26 14:25:45 -0400

    Update uptycs_aws_cspm.tf

--- sandbox/terraform/uptycs_aws_cspm.tf
+++ sandbox/terraform/uptycs_aws_cspm.tf
@@ -3,6 +3,7 @@ locals {
     "646602658615" = {
       external_id        = "980b2db9-067e-5926-ae49-ba9022ee0e08",
       integration_prefix = "GiriUptycsIntegration"
+    }
   }
   default_values = {
     external_id        = "557a929b-39a5-4335-9fb0-c005f9cd44c3"
