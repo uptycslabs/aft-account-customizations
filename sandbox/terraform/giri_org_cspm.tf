4c0576e560533b706d6325725e63dd821aaeb3d7
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-10-13 11:52:10 -0400

    Create giri_org_cspm.tf

---
+++ sandbox/terraform/giri_org_cspm.tf
@@ -0,0 +1,25 @@
+module "uptycs_aws_cspm_cqmaster" {
+    source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
+    integration_prefix              = "cqmasterstack"
+    upt_account_id                  = "685272795239"
+    external_id                     = "2576af75-395b-5f4c-a063-a84f90cf1a33"
+    permissions_boundary_policy_arn = ""
+}
+
+module "uptycs_aws_cspm_giri" {
+    source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
+    integration_prefix              = "giristack"
+    upt_account_id                  = "685272795239"
+    external_id                     = "c161b4cc-b028-5e18-b2cb-7a81e37db881"
+    permissions_boundary_policy_arn = ""
+}
+
+/*
+module "uptycs_aws_cspm_cqrelease" {
+    source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
+    integration_prefix              = "cqreleasestack"
+    upt_account_id                  = "685272795239"
+    external_id                     = "2576af75-395b-5f4c-a063-a84f90cf1a33"
+    permissions_boundary_policy_arn = ""
+}
+*/
