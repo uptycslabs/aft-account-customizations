46522717f8ccaa9b0f450350bccc6529d92eb119
Giri Kondisetty <67404190+gkondisetty-uptycs@users.noreply.github.com>
2024-10-14 18:35:22 -0400

    Update giri_org_cspm.tf

--- sandbox/terraform/giri_org_cspm.tf
+++ sandbox/terraform/giri_org_cspm.tf
@@ -1,6 +1,7 @@
+/*
 module "uptycs_aws_cspm_cqmaster" {
     source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
-    integration_prefix              = "cqmasterstack"
+    integration_prefix              = "cqmasterstack1"
     upt_account_id                  = "685272795239"
     external_id                     = "2576af75-395b-5f4c-a063-a84f90cf1a33"
     permissions_boundary_policy_arn = ""
@@ -8,16 +9,15 @@ module "uptycs_aws_cspm_cqmaster" {
 
 module "uptycs_aws_cspm_giri" {
     source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
-    integration_prefix              = "giristack"
+    integration_prefix              = "giristack1"
     upt_account_id                  = "685272795239"
     external_id                     = "c161b4cc-b028-5e18-b2cb-7a81e37db881"
     permissions_boundary_policy_arn = ""
 }
 
-/*
 module "uptycs_aws_cspm_cqrelease" {
     source                          = "https://uptycs-terraform-dev.s3.amazonaws.com/terraform-aws-uptycs.zip//modules/cspm_integration"
-    integration_prefix              = "cqreleasestack"
+    integration_prefix              = "cqreleasestack1"
     upt_account_id                  = "685272795239"
     external_id                     = "2576af75-395b-5f4c-a063-a84f90cf1a33"
     permissions_boundary_policy_arn = ""
